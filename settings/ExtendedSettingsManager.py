0866cfe2793db11e60679d6fbcd14b30ab812a5a
jawadefaj <jawadefaj006@gmail.com>
2022-06-17 16:43:57 -0700

    added function to extended setting manager

--- settings/ExtendedSettingsManager.py
+++ settings/ExtendedSettingsManager.py
@@ -75,3 +75,42 @@ class CogModSettingsManager(SettingsManager):
         return (numberOfAgents, actor_trajectory_list)
 
 
+    def getStraightRoadSimulationSettings(self):
+
+        # cogmod agent settings
+        cogmod_agent_settings = self.currentSetting["cogmod_agent"]
+
+        cogmod_spawn_coordinate= cogmod_agent_settings["spawn_point"]
+        cogmod_destination_coordinate = cogmod_agent_settings["destination_point"] 
+
+        cogmod_spawn_transform = self.convertCoordinateToTransform(cogmod_spawn_coordinate)
+        cogmod_destination_transform = self.convertCoordinateToTransform(cogmod_destination_coordinate)
+
+        cogmod_agent_settings = {
+            "spawn_transform": cogmod_spawn_transform,
+            "destination_transform": cogmod_destination_transform,
+            "driver_profile": cogmod_agent_settings["driver_profile"]
+        }
+
+        # actor agent settings
+        actor_agent_settings = self.currentSetting["actor_agent"]
+
+        actor_spawn_coordinate = actor_agent_settings["spawn_point"]
+        actor_destination_coordinate = actor_agent_settings["destination_point"]
+
+        actor_spawn_transform = self.convertCoordinateToTransform(actor_spawn_coordinate)
+        actor_destination_transform = self.convertCoordinateToTransform(actor_destination_coordinate)
+
+        actor_agent_settings = {
+            "spawn_transform": actor_spawn_transform,
+            "destination_transform": actor_destination_transform,
+            "driver_profile": actor_agent_settings["driver_profile"],
+            "target_speed": actor_agent_settings["target_speed"]
+        }
+
+        trigger_distance = self.currentSetting["trigger_distance"]
+
+        return (cogmod_agent_settings, actor_agent_settings, trigger_distance)
+
+
+
